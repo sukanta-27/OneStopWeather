b19ff84364934d70dfa3bf61536ddb9058a66845
sukanta-27 <sukanta.raj.27@gmail.com>
2021-02-26 04:17:28 +0530

    Add navbar - very basic ugly one

--- app.py
+++ app.py
@@ -44,5 +44,10 @@ def index():
 
     return render_template('index.html', weather=weather)
 
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    pass
+
 if __name__ == '__main__':
     app.run(debug=True)
