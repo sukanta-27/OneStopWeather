d74c4439c8646fc00c25b5a69ed647aeeb7c45e4
sukanta-27 <sukanta.raj.27@gmail.com>
2021-02-27 12:28:03 +0530

    Add API key check in weatherInfo and remove commented code from style.css

--- util/WeatherInfo.py
+++ util/WeatherInfo.py
@@ -5,6 +5,10 @@ import os
 class WeatherInfo:
     
     def __init__(self, city=None, country=None, zip=None, units='metric'):
+
+        # First check API key is set in .env file
+        self.checkAPIKey()
+
         self.city = city
         if country:
             self.country = country.title()
@@ -13,6 +17,12 @@ class WeatherInfo:
         self.zip = zip
         self.units = units
     
+    def checkAPIKey(self):
+        if os.getenv('API_KEY'):
+            return True
+        else:
+            raise "API key not found in .env file"
+
     def retrieveCountryCode(self):
         if not self.country:
             return None
